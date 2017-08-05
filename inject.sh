90e0ebc5d31d62ea502abaa9c91d738776c8f8bc
nullifiedcat <nullifiedcat@gmail.com>
2017-08-05 14:37:26 +0300

    update inject.sh to randomize filename

--- inject.sh
+++ inject.sh
@@ -5,17 +5,29 @@ if ! [[ $proc =~ ^[0-9]+$ ]]; then
    echo "Couldn't find process! Are you sure a game is running?" >&2; exit 1
 fi
 
-if grep -q "libcathook.so" /proc/"$proc"/maps; then
-  echo already loaded
-  exit
-fi
+sudo rm -rf /tmp/dumps # Remove if it exists
+sudo mkdir /tmp/dumps # Make it as root
+sudo chmod 000 /tmp/dumps # No permissions
 
-echo loading "$(realpath cathook/libcathook.so)" to "$proc"
+FILENAME="/tmp/.gl$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 6 | head -n 1)"
+
+cp "cathook/libcathook.so" "$FILENAME"
+
+echo loading "$FILENAME" to "$proc"
+
+sudo killall -19 steam
+sudo killall -19 steamwebhelper
+
 gdb -n -q -batch \
   -ex "attach $proc" \
   -ex "set \$dlopen = (void*(*)(char*, int)) dlopen" \
-  -ex "call \$dlopen(\"$(realpath cathook/libcathook.so)\", 1)" \
+  -ex "call \$dlopen(\"$FILENAME\", 1)" \
   -ex "call dlerror()" \
   -ex 'print (char *) $2' \
   -ex "detach" \
   -ex "quit"
+  
+rm $FILENAME
+
+sudo killall -18 steamwebhelper
+sudo killall -18 steam
