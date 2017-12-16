958b553e46a3c5aaac504d262fa93a8c1f93908b
nullifiedcat <nullifiedcat@gmail.com>
2017-12-16 13:38:41 +0300

    fix typo

--- 07-install-config.sh
+++ 07-install-config.sh
@@ -10,7 +10,7 @@ if [ $EUID == 0 ]; then
 fi
 
 sudo mkdir -p "/opt/cathook/data"
-sudo chmod 777 "/opt/cathook-data"
+sudo chmod 777 "/opt/cathook/data"
 
 cp cat_autoexec_textmode.cfg "/opt/steamapps/common/Team Fortress 2/tf/cfg"
 cp botspam "/opt/cathook/data"
