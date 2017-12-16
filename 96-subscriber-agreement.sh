350f1a062122a25bb06fa135c879f45276c28559
nullifiedcat <nullifiedcat@gmail.com>
2017-12-16 12:57:31 +0300

    SSA

--- 96-subscriber-agreement.sh
+++ 96-subscriber-agreement.sh
@@ -5,5 +5,6 @@ read -p "Press ENTER to continue"
 for i in $(seq 1 $max)
 do
 	echo "catbot-$i has read the Steam SA"
-	sudo -H -u catbot-$i bash -c "cp ~/.steam/steam/steam_install_agreement.txt ~/.steam"
+	sudo -H -u catbot-$i bash -c "mkdir -p ~/.steam"
+	sudo -H -u catbot-$i bash -c "touch ~/.steam/steam_install_agreement.txt"
 done
