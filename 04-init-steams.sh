105193b5f473ba3ac0ae14713806770d483dbb39
RaggedCaster <30390927+RaggedCaster@users.noreply.github.com>
2017-12-27 00:04:49 +0300

    better 04-04-init-steams.sh
    improvement:can launch a range of catbots from #N to #M

--- 04-init-steams.sh
+++ 04-init-steams.sh
@@ -2,18 +2,22 @@
 
 #
 #	This script starts N steam instances, 6 by default
-#
+#	Can also be used to start instances #N-#M
 
+min=1
 max=6
 
 if [ "$#" == 1 ]; then
 	max=$1
+elif [ "$#" == 2 ]; then
+	min=$1
+	max=$2	
 fi
 
 # Required for steam to start as other users
 xhost + >/dev/null
 
-for i in $(seq 1 $max)
+for i in $(seq $min $max)
 do
 	echo "Starting Steam for catbot $i"
 	sudo su - catbot-$i -c "steam &>/tmp/steam-catbot-$i.log 2>&1 &"
