953b71eb8ddd8bd3026d4b3df183f91d7397255f
Marc <git@marcsteiner.me>
2017-12-30 23:19:21 +0100

    Only lowercase usernames are allowed

--- 00-generate-random-username.sh
+++ 00-generate-random-username.sh
@@ -4,5 +4,5 @@
 # Script generates random 16-char length string that will be used for usernames
 #
 
-name=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
+name=$(cat /dev/urandom | tr -dc 'a-z0-9' | fold -w 16 | head -n 1)
 echo $name > kisak
