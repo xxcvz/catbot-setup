2850ab60b1d390255d141f15108df23614905755
SylveonBottle <depressedeevee@gmail.com>
2019-06-21 03:00:24 -0500

    Remove old scripts

--- old/00-generate-random-username.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#
-# Script generates random 6-char length string that will be used for usernames
-#
-
-name=$(cat /dev/urandom | tr -dc 'a-z' | fold -w 6 | head -n 1)
-echo $name > kisak
--- old/95-harvest.sh
+++
@@ -1,4 +0,0 @@
-sudo mkdir -p /opt/cathook/data/walkbot/koth_harvest_final
-sudo cp harvest.catwalk /opt/cathook/data/walkbot/koth_harvest_final/default
-sudo cp casual_criteria.vdf /opt/steamapps/common/Team\ Fortress\ 2/tf
-sudo chmod 777 /opt/steamapps/common/Team\ Fortress\ 2/tf/casual_criteria.vdf
